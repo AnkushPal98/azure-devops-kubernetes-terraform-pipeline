5813f78e99d707e16aafa3469fef81c6ae6bab09
AnkushPal98 <ankkpundeer123@gmail.com>
2023-12-21 20:41:01 +0530

    adding sensitive property inside output resource

--- configuration/iaac/azure/kubernetes/outputs.tf
+++ configuration/iaac/azure/kubernetes/outputs.tf
@@ -1,20 +1,25 @@
 output "client_key" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_key
 }
 
 output "client_certificate" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.cluster_ca_certificate
 }
 
 output "cluster_username" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.username
 }
 
 output "cluster_password" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.password
 }
 
@@ -24,5 +29,6 @@ output "kube_config" {
 }
 
 output "host" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host
 }
